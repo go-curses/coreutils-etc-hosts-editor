a0034c3b78f2ae6b7438ff428eb0e18507443e92
Kevin C. Krinke <kevin@krinke.ca>
2024-01-21 14:48:31 -0500

    cmd: purging deprecated cdk.Build Include* passthroughs

--- cmd/eheditor/main.go
+++ cmd/eheditor/main.go
@@ -19,44 +19,15 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/go-curses/cdk"
-	cstrings "github.com/go-curses/cdk/lib/strings"
 	"github.com/go-curses/cdk/log"
 
 	"github.com/go-curses/coreutils-etc-hosts-editor/ui/eheditor"
-)
-
-// Build Configuration Flags
-// setting these will enable command line flags and their corresponding features
-// use `go build -v -ldflags="-X 'main.IncludeLogFullPaths=false'"`
-var (
-	IncludeProfiling          = "false"
-	IncludeLogFile            = "false"
-	IncludeLogFormat          = "false"
-	IncludeLogFullPaths       = "false"
-	IncludeLogLevel           = "false"
-	IncludeLogLevels          = "false"
-	IncludeLogTimestamps      = "false"
-	IncludeLogTimestampFormat = "false"
-	IncludeLogOutput          = "false"
 )
 
 var (
 	BuildVersion = "0.5.6"
 	BuildRelease = "trunk"
 )
-
-func init() {
-	cdk.Build.Profiling = cstrings.IsTrue(IncludeProfiling)
-	cdk.Build.LogFile = cstrings.IsTrue(IncludeLogFile)
-	cdk.Build.LogFormat = cstrings.IsTrue(IncludeLogFormat)
-	cdk.Build.LogFullPaths = cstrings.IsTrue(IncludeLogFullPaths)
-	cdk.Build.LogLevel = cstrings.IsTrue(IncludeLogLevel)
-	cdk.Build.LogLevels = cstrings.IsTrue(IncludeLogLevels)
-	cdk.Build.LogTimestamps = cstrings.IsTrue(IncludeLogTimestamps)
-	cdk.Build.LogTimestampFormat = cstrings.IsTrue(IncludeLogTimestampFormat)
-	cdk.Build.LogOutput = cstrings.IsTrue(IncludeLogOutput)
-}
 
 func main() {
 	ehe := eheditor.NewEheditor(
