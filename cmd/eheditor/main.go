3ab1619ec35b3cd15b0e8812190cebac083f57de
Kevin C. Krinke <kevin@krinke.ca>
2024-01-22 10:39:59 -0500

    cmd: use go-corelibs/cli.FlagStringer; use go-corelibs/cli.ClearEmptyCategories; remove cdk tty flag category setting

--- cmd/eheditor/main.go
+++ cmd/eheditor/main.go
@@ -19,7 +19,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/go-curses/cdk"
+	clcli "github.com/go-corelibs/cli"
 	"github.com/go-curses/cdk/log"
 
 	"github.com/go-curses/coreutils-etc-hosts-editor/ui"
@@ -31,7 +31,9 @@ var (
 )
 
 func init() {
-	cdk.AppCliTtyFlag.Category = ""
+	cli.FlagStringer = clcli.NewFlagStringer().
+		PruneDefaultBools(true).
+		Make()
 }
 
 func main() {
@@ -59,6 +61,7 @@ func main() {
 		Usage:   "display the version",
 		Aliases: []string{"v"},
 	}
+	clcli.ClearEmptyCategories(appCLI.Flags)
 	if err := ehe.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
